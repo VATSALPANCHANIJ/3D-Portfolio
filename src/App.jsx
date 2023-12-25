5b59b8824731bf4c7cace4e6bc648f7813ac895b
VATSAL Panchani <reactdeveloper3010@gmail.com>
2023-12-25 10:50:31 +0530

    section_1_done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.scss";
-import Test from "./Test";
-
-import Navbar from "./components/Navbar/Navbar"
+// import Test from "./Test";
+import Hero from "./components/hero/hero";
+import Navbar from "./components/Navbar/Navbar";
 const App = () => {
   return (
     <>
       <section id="HomePage">
         <Navbar></Navbar>
+        <Hero></Hero>
       </section>
-      <section id="Services">information</section>
-      <section>information</section>
-      <section id="Portfolio">information</section>
-      <section>information</section>
-      <section>information</section>
-      <section id="Contact">Contact</section>
+      <section id="Services">Services</section>
+      <section id="Portfolio">Portfolio</section>
+      <section id="Content">Content</section>
+      <section id="About">About</section>
+
       {/* <Test></Test>
       <Test></Test>
       <Test></Test> */}
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import './Hero.scss';
+import { motion } from "framer-motion";
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        },
+    },
+    scrollButton: {
+        opacity: 0,
+        y: 10,
+        transition: {
+            duration: 2,
+            repeat: Infinity
+        }
+    },
+};
+const sliderVariants = {
+    initial: {
+        x: 0,
+    },
+    animate: {
+        x: "-220%",
+        transition: {
+            repeat: Infinity,
+            repeatType:"mirror",
+            duration: 20,
+        },
+    },
+
+};
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="scondmain">
+
+                <div className="wrapper">
+
+                    <motion.div className="text-Container"
+                        variants={textVariants}
+                        initial="initial"
+                        animate='animate'
+                    >
+                        <motion.h2 variants={textVariants}>Vatsal Panchani</motion.h2>
+
+                        <motion.h1 variants={textVariants}>Web developer</motion.h1>
+
+                        <motion.div variants={textVariants} className="buttons">
+
+                            <motion.button variants={textVariants}>See Latest Works</motion.button>
+
+                            <motion.button variants={textVariants}>Contact Me</motion.button>
+
+                        </motion.div>
+                        <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="button scroll" />
+
+                    </motion.div>
+
+                    <motion.div className="slidingTextContainer" variants={sliderVariants}
+                        initial="initial"
+                        animate="animate"
+                        >
+                        Write Content Creator Influencer
+                    </motion.div>
+
+                    <div className="imageContainer">
+                        <img src="/hero.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
